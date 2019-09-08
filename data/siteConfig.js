199fe91fe7d242a21e5ad648588ac13c052a785a
sjoonwai <sjoonwai@gmail.com>
2019-09-08 20:00:44 +0800

    Project first commit

---
+++ data/siteConfig.js
@@ -0,0 +1,161 @@
+module.exports = {
+  siteTitle: "Hi! I'm Marcus!",
+  siteDescription: `Portfolio site for Marcus Soon Joon Wai`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Marcus Soon',
+  twitterUsername: '_franciscodf',
+  linkedinURL: 'https://www.linkedin.com/in/joon-wai-s-819a5364/',
+  authorAvatar: '/images/marcus.jpg',
+  authorDescription: `Enthusiastic web developer with proficient knowledge in full stack application development.
+  Acknowledges the importance of testing and have a good sense in debugging process. An avid learner, constantly motivated to learn new technologies and keeping up-to-date with latest market trends.
+  Able to effectively self-manage during independent projects, as well as collaborate in a team setting.`,
+  skills: [
+    {
+      name: 'C#',
+      level: 80,
+    },
+    {
+      name: 'ASP.NET',
+      level: 75,
+    },
+    {
+      name: 'Javascript',
+      level: 80,
+    },
+    {
+      name: 'jQuery',
+      level: 75,
+    },
+    {
+      name: 'Git',
+      level: 70,
+    },
+    {
+      name: 'Jenkins',
+      level: 75,
+    },
+    {
+      name: 'DevExpress',
+      level: 80,
+    },
+    {
+      name: 'HTML',
+      level: 80,
+    },
+    {
+      name: 'CSS',
+      level: 75,
+    },
+    {
+      name: 'MySQL',
+      level: 75,
+    },
+    {
+      name: 'Adobe Photoshop',
+      level: 70,
+    },
+    {
+      name: 'Google Suite',
+      level: 70,
+    },
+    {
+      name: 'Microsoft Office',
+      level: 70,
+    },
+  ],
+  jobs: [
+    {
+      company: 'TimeTec Computing',
+      begin: {
+        month: 'Aug',
+        year: '2017',
+      },
+      duration: null,
+      occupation: 'Analyst Programmer',
+      descriptions: [
+        {
+          data:
+            'Developed and maintained ASP.NET cloud web applications hosted on Amazon EC2 platform.',
+        },
+        {
+          data:
+            'Translated business requirements into technical and system specifications.',
+        },
+        {
+          data:
+            'Conducted full stack development using DevExpress as front-end framework and C# as back-end.',
+        },
+        {
+          data:
+            'Created RESTful APIs for mobile and web clients using .NET Core.',
+        },
+        {
+          data:
+            'Experienced in client-server architecture and microservices architecture.',
+        },
+        { data: 'Performed unit testing using NUnit and Specflow.' },
+        {
+          data:
+            'Implemented CI/CD using Jenkins by setting up pipeline and designing automated workflow.',
+        },
+        { data: 'Written guidelines and research documents for internal use.' },
+      ],
+    },
+    {
+      company: 'TimeTec Computing',
+      begin: {
+        month: 'feb',
+        year: '2017',
+      },
+      duration: '6 months',
+      occupation: 'Intern',
+      descriptions: [
+        {
+          data:
+            'Worked on mobile server side; processing business logic at back-end and returning results to mobile app client in JSON format.',
+        },
+        {
+          data:
+            'Built class libraries and user controls for shared use among different web application projects.',
+        },
+        {
+          data:
+            'Performed web application enhancements based on given requirement.',
+        },
+      ],
+    },
+    {
+      company: 'Malaysiakini',
+      begin: {
+        month: 'jan',
+        year: '2013',
+      },
+      duration: '8 months',
+      occupation: 'Junior Graphic Editor',
+      descriptions: [{ data: 'Create graphics to be included in news article comprising of charts, graphs and montages.' },
+    {data:'Learned how to use Adobe Photoshop and developed a sense of creative design.'},
+  {data:'Experienced working in a fast-paced environment; learned how to communicate and co-operate with team members.'}],
+    },
+    /* ... */
+  ],
+  social: {
+    twitter: 'https://twitter.com/_franciscodf',
+    linkedin: 'https://www.linkedin.com/in/joon-wai-s-819a5364/',
+    github: 'https://github.com/sjoonwai',
+    email: 'sjoonwai@gmail.com',
+  },
+  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/bg4.jpg',
+  googleAnalyticsId: 'UA-000000000-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Marcus Soon',
+      url: '/',
+    },
+  ],
+}
